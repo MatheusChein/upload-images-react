ae567abb210b637b0761f43a96b06f816d20a710
Matheus Chein <matheus.chein@fabric.inc>
2022-01-20 21:15:25 -0300

    4 tests passing

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,24 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent background="none">
+        <ModalBody padding="0">
+          <Image src={imgUrl} width="100%" maxW="900px" maxHeight="600px" />
+        </ModalBody>
+
+        <ModalFooter
+          background="pGray.800"
+          borderRadius="0 0 20px 20px"
+          justifyContent="flex-start"
+        >
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -18,26 +19,70 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get(
+        `/api/images${pageParam ? `?after=${pageParam}` : ''}`
+      );
+
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => {
+        return lastPage.after;
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    // const formatted = data?.pages.flatMap(page => page.data);
+
+    // return formatted;
+
+    return data?.pages.reduce((acc, page) => {
+      acc = acc?.concat(page.data);
+      return acc;
+    }, []);
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return (
+      <>
+        <Loading />;
+      </>
+    );
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return (
+      <>
+        <Error />
+      </>
+    );
+  }
 
   return (
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box
+        maxW={1120}
+        px={20}
+        mx="auto"
+        my={20}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            width="10rem"
+            marginTop="2rem"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
